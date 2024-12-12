5e87c725605c511798ffdcbbe220540796db0690
katerynachornukha <kateryna.chornukha@nure.ua>
2024-12-12 17:25:59 +0200

    task1.py

--- task1.py
+++ task1.py
@@ -3,9 +3,9 @@ import requests
 import json
 from requests import request
 
-# https://bank.gov.ua/NBU_Exchange/exchange_site?start=20240916&end=20240920&valcode=usd&json
+# https://bank.gov.ua/NBU_Exchange/exchange_site?start=20241202&end=20241208&valcode=usd&json
 
-reply = requests.get("https://bank.gov.ua/NBU_Exchange/exchange_site?start=20240916&end=20240920&valcode=usd&json ")
+reply = requests.get("https://bank.gov.ua/NBU_Exchange/exchange_site?start=20241202&end=20241208&valcode=usd&json ")
 
 reply_json = json.loads(reply.text)
 
